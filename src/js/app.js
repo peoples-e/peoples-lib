4f9d12921ab57d82b47fd4f235ff011109d1d5d1
Oliver Wise <owise1@gmail.com>
2014-07-23 16:12:46 -0700

    uploads doc, closes #8

--- src/js/app.js
+++ src/js/app.js
@@ -7,12 +7,29 @@ var PeoplesLibApp = angular.module('PeoplesLibApp', ['CornerCouch']);
 
 PeoplesLibApp.controller('ListingCtrl', ['$scope', '$http', 'cornercouch', function ($scope, $http, cornercouch) {
   var config = window._pl;
-
   $scope.db = cornercouch().getDB(config.db);
+
+  $scope.upload = function(elem){
+    var files = [];
+    for (var i = 0, f; f = elem.files[i]; i++) {
+      files.push(f);
+    }
+    files.forEach(function(f){
+      var doc = $scope.db.newDoc({
+        name : f.name,
+        content_type : f.type
+      })
+      doc.save()
+      .success(function(){
+        doc.attach(f, f.name); 
+      });
+    })
+  }
+
+  // get listing
   $scope.db.query(config.design, 'media', { reduce : false, limit : 10, include_docs : true })
   .success(function(resp){
     $scope.medias = resp.rows.map(processDoc);
-    console.log($scope.medias);
   });
 
   function processDoc(row){
