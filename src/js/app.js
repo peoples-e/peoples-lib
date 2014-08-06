259a5cc383b929548c2615de41011f649c23a4a1
Oliver Wise <owise1@gmail.com>
2014-08-06 16:00:14 -0700

    integrates pouchDB closes #5

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,52 @@ var PeoplesLibApp = angular.module('PeoplesLibApp', ['CornerCouch']);
 
 PeoplesLibApp.controller('ListingCtrl', ['$scope', '$http', 'cornercouch', function ($scope, $http, cornercouch) {
   var config = window._pl;
-  $scope.db = cornercouch().getDB(config.db);
+  $scope.db      = cornercouch().getDB(config.db);
+  $scope.localDb = new PouchDB(config.db);
+
+  function showLocalMedia(){
+    $scope.localDb.query(config.design + '/media', { reduce : false, limit : 10, include_docs : true }, function(err, resp){
+      $scope.$apply(function(){
+        $scope.localMedias = resp.rows.map(processDoc);
+      })
+    });
+  }
+
+  $scope.localDb.get('_design/peoples-lib', function(err, res){
+    if(res){
+      showLocalMedia();
+    } else {
+      $scope.localDb.replicate.from(window.location.origin + '/' + config.db, { doc_ids : '_design/' + config.design } ,showLocalMedia);
+
+    }
+  });
+ 
+  $scope.sync = function(doc){
+      doc.__syncing = true;
+      $scope.localDb.replicate.from(window.location.origin + '/' + config.db, { doc_ids : [doc._id] } ,function(err, res){
+        showLocalMedia();
+        doc.__syncing = false;
+      });
+  }
+
+  $scope.remove = function(doc){
+    // pouchDB does not currently support _purge (http://wiki.apache.org/couchdb/Purge_Documents)
+    // so we have to create a new db wo/ the doc 
+    // cuz otherwise, once we deleted a doc, we'd never be able to get it again
+    $scope.localDb.allDocs(function(err, res){
+      var remainingDocs = res.rows.map(function(d){ return d.id; }).filter(function(d){ return d !== doc._id });
+      var tmpDB         = new PouchDB('tmp');
+      tmpDB.replicate.from($scope.localDb, { doc_ids : remainingDocs }, function(err, res){
+        $scope.localDb.destroy(function(){
+          $scope.localDb = new PouchDB(config.db);
+          $scope.localDb.replicate.from(tmpDB, function(){
+            tmpDB.destroy();
+            showLocalMedia();
+          });
+        });
+      });
+    }); 
+  }
 
   $scope.upload = function(elem){
     var files = [];
